e401e05e52c9a6a84578749edaf1379ffaebfc78
kgs90 <kgs90@inbox.ru>
2022-07-22 20:52:15 +0300

    task4

---
+++ task4.py
@@ -0,0 +1,10 @@
+n = int(input('Введите число: '))
+
+def conv_dec_to_bin(n):
+    bin_num = ''
+    while n > 1:
+        bin_num += str(n % 2)
+        n = n // 2
+    return bin_num[::-1]
+
+print(conv_dec_to_bin(n))
